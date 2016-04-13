5f81342b537a6fea41f7c99c336f9caf3352d7cb
Brian Nelson <brian@treegecko.com>
2016-04-13 05:05:36 -0700

    Adjusted command line

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-c:\projects\csharphdf5\src\CSharpHDF5\packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe  /xml:"c:\projects\results-test.xml" "c:\projects\csharphdf5\src\CSharpHDF5Tests\bin\x64\Debug\CSharpHDF5Tests.dll"
+c:\projects\csharphdf5\src\CSharpHDF5\packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe --result=c:\projects\results-test.xml;format=nunit2 "c:\projects\csharphdf5\src\CSharpHDF5Tests\bin\x64\Debug\CSharpHDF5Tests.dll"
