adb48e75b10cf5187389e91be7ce4488f5a92eae
Brian Nelson <brian@treegecko.com>
2016-04-12 21:47:26 -0700

    fixes

--- cover.bat
+++ cover.bat
@@ -1,6 +1,6 @@
 c:\projects\csharphdf5\src\CSharpHDF5\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe ^
 	-register:user ^
-	-output:test\results-coverage.xml ^
+	-output:c:\projects\csharphdf5\csharphdf5-coverage.xml ^
 	"-filter:+[CSharpHDF5]*  -[CSharpHDF5]CSharpHDF5.Properties.*" ^
 	-excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" ^
 	"-target:c:\projects\csharphdf5\test.bat"
