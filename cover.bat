5a5286ca21457c77d69c9c4fe296ce1590954b21
Brian Nelson <brian@treegecko.com>
2016-04-12 21:28:48 -0700

    moved to batch files

---
+++ cover.bat
@@ -0,0 +1,6 @@
+c:\projects\csharphdf5\src\CSharpHDF5\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe ^
+	-register:user ^
+	-output:test\results-coverage.xml ^
+	"-filter:+[CSharpHDF5]*  -[CSharpHDF5]CSharpHDF5.Properties.*" ^
+	-excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" ^
+	"-target:c:\projects\csharphdf5_coverage.xml"
---
+++ test.bat
@@ -0,0 +1,5 @@
+c:\projects\csharphdf5\src\CSharpHDF5\packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe ^
+    /noshadow ^
+    /nologo ^
+    /xml:c:\projects\results-test.xml ^
+    c:\projects\csharphdf5\src\CSharpHDF5Tests\bin\x64\Debug\CSharpHDF5Tests.dll
