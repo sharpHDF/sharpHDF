2f7905d9055d17c3b797d64a30bf4f1b2887784e
Brian Nelson <brian@treegecko.com>
2016-04-15 22:05:45 -0700

    fix

--- test.bat
+++ test.bat
@@ -1,1 +1,3 @@
-c:\projects\sharphdf\src\sharpHDF\packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe --result=c:\projects\results-test.xml;format=nunit2 "c:\projects\sharphdf\src\sharpHDFTests\bin\x64\Debug\sharpHDFTests.dll"
+c:\projects\sharphdf\src\sharpHDF\packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe  ^
+	--result=c:\projects\results-test.xml;format=nunit2  ^
+	"c:\projects\sharphdf\src\sharpHDFTests\bin\x64\Debug\sharpHDFTests.dll"
